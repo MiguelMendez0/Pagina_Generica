bf75de36234103830bb80fc7891f30e3dc64d814
MiguelMendez0 <miguel_mendez_cerino@outlook.com>
2025-02-19 18:24:15 -0600

    conexion db

---
+++ CONTROLLERS/connection-db.js
@@ -0,0 +1,18 @@
+let mysql = require("mysql");
+
+let conexion = mysql.createConnection({
+    host:"127.0.0.1",
+    user:"root",
+    password:"Fastnet#98",
+    database:"generic",
+    port:3307
+}); 
+
+conexion.connect(function(err){
+    if(err){
+        throw err;
+    }else{
+        console.log("Conexion exitosa")
+    }
+});
+
